19ffbf83db052467170ae2824791f32d7afcdf0b
Anand prabhakar <anandprabhakar0507@gmail.com>
2018-10-28 20:30:49 -0700

    Create print prime in interval.py

---
+++ print prime in interval.py
@@ -0,0 +1,17 @@
+# Python program to display all the prime numbers within an interval
+
+# change the values of lower and upper for a different result
+lower = 1
+upper = 100
+
+
+print("Prime numbers between",lower,"and",upper,"are:")
+
+for num in range(lower,upper + 1):
+   # prime numbers are greater than 1
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
